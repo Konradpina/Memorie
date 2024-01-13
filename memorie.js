ea0c971b6c2fa927ae2693771258cb9bbbdb82af
Konradpina <ksentek@gmx.de>
2024-01-13 17:37:41 +0100

    Memory

--- memorie.js
+++ memorie.js
@@ -2,16 +2,20 @@ const coloroptions=["red","green","black","blue","purple","yellow","pink","orang
 var count=false
 var card1
 var card2
+var level=1
 
 function asingcolor(){
     var cards= document.getElementsByClassName("Card")
+    var html=""
     var options=coloroptions
-    for(i=0;i<cards.length;i++){
+    for(i=0;i<16;i++){
         var r = Math.floor(Math.random()*options.length)
-        document.getElementsByClassName("Card")[i].style=`background-color: ${options[r]}`
-        console.log(cards[i])
+        html=`${html} <div class="gridbit"><div class="Card" style="background-color:${options[r]}" onclick="pick(${i+1})"></div></div>`
         options.splice(r,1)
+        console.log(html)
     }
+    console.log(html)
+    document.getElementById("Grid").innerHTML=html
 }
 asingcolor()
 
@@ -44,19 +48,27 @@ function eval(){
     var cards= document.getElementsByClassName("Card")
     let card11=card1
     let card22=card2
-    if(cards[card1].style.backgroundColor===cards[card2].style.backgroundColor){
+    
+    if(cards[card1].style.backgroundColor===cards[card2].style.backgroundColor&&cards[card11].style.backgroundColor!="grey"&&cards[card22].style.backgroundColor!="grey"){
         setTimeout(()=>{
             cards[card11].style.backgroundColor="#fff"
             cards[card22].style.backgroundColor="#fff"
-            document.getElementsByClassName("Card")[card11].removeAttribute("onclick")
-            document.getElementsByClassName("Card")[card22].removeAttribute("onclick")
+            if(level===0){
+                document.getElementsByClassName("Card")[card11].removeAttribute("onclick")
+                document.getElementsByClassName("Card")[card22].removeAttribute("onclick")
+            }else if(level===1){
+                cards[card11].style.backgroundColor="grey"
+                cards[card22].style.backgroundColor="grey"
+                cards[card11].classList.remove("Opacity1")
+                cards[card22].classList.remove("Opacity1")
+            }
             document.getElementById("right").load()
             document.getElementById("right").play()
             document.getElementById("rightcount").innerHTML=Number(document.getElementById("rightcount").innerHTML)+1
+            if(innerHTML=Number(document.getElementById("rightcount").innerHTML)===8){
+                rest()
+            }
         },1000)
-        if(innerHTML=Number(document.getElementById("rightcount").innerHTML)===8){
-            rest()
-        }
 
     }else{
         setTimeout(()=>{
@@ -66,6 +78,7 @@ function eval(){
             document.getElementById("wrong").play()
             cards[card11].classList.remove("Opacity1")
             cards[card22].classList.remove("Opacity1")
+            
             document.getElementById("wrongcount").innerHTML=Number(document.getElementById("wrongcount").innerHTML)+1
         },1000)
     }
